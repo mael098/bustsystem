751586efd6263c31925bee8cabe309f7bca0eff9
mael098 <juanvillegasmorales@outlook.com>
2026-04-01 22:52:19 -0600

    integracion de login ami app movil

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+      name='login'
+      options={{
+        title: 'login',
+        tabBarIcon:({color}) => <IconSymbol size={28} name='0.circle.fill' color={color} />,
+      }}
+
+      />
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Stack } from 'expo-router';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -16,7 +16,9 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="login" options={{headerShown:false}}/> */}
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
